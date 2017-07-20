3e064f191fc35fd37d5a1b56d312132fee48cb0e
arvit <avarfaj86@gmail.com>
2017-07-20 23:57:44 +0200

    init commit

---
+++ src/main/java/org/javasavvy/hook/portlet/actions/CustomLoginActionActivator.java
@@ -0,0 +1,19 @@
+package org.javasavvy.hook.portlet.actions;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author nushi
+ */
+public class CustomLoginActionActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
---
+++ src/main/java/org/javasavvy/hook/portlet/actions/CustomLoginActionCommand.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.javasavvy.hook.portlet.actions;
+
+import javax.portlet.PortletPreferences;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.login.web.internal.portlet.util.LoginUtil;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.events.LifecycleEvent;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.security.auth.session.AuthenticatedSessionManager;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+@Component(immediate = true, property = { "key=login.events.pre" }, service = LifecycleAction.class)
+public class CustomLoginActionCommand implements LifecycleAction {
+
+	@Override
+	public void processLifecycleEvent(LifecycleEvent lifecycleEvent)
+		throws ActionException {
+    	
+    	HttpServletRequest request = lifecycleEvent.getRequest();
+    	String portletId = _portal.getPortletId(request);
+    	
+    	ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+    			WebKeys.THEME_DISPLAY);
+
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.getStrictPortletSetup(
+				themeDisplay.getLayout(), portletId);
+
+		String authType = portletPreferences.getValue("authType", null);
+		System.out.println("///////////// " + request.getParameter("login"));
+    	try {
+			LoginUtil.getAuthenticatedUserId(request, request.getParameter("login"), request.getParameter("password"), authType);
+		} catch (PortalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("This is my addin ///////////////// login.event.pre=" + lifecycleEvent);
+	}
+
+	@Reference
+	private AuthenticatedSessionManager _authenticatedSessionManager;
+
+	@Reference
+	private Portal _portal;
+}
